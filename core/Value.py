702ddb467c184e5d1f8dd5b0d8f78e2923987cea
Himanshu Yadav <asherkingpin@gmail.com>
2026-03-16 15:25:47 +0530

    Initial commit: mini_torch autograd engine

---
+++ core/Value.py
@@ -0,0 +1,96 @@
+from math import sin, cos, exp, log
+import math
+
+
+class Value:
+
+  def __init__(self, data, _children=(), _op='', label=''):
+    self.data = data
+    self.grad = 0.0
+    self._backward = lambda: None
+    self._prev = set(_children)
+    self._op = _op
+    self.label = label
+
+  def __repr__(self):
+    return f"Value(data={self.data})"
+
+  def __add__(self, other): # exactly as in the video
+    other = other if isinstance(other, Value) else Value(other)
+    out = Value(self.data + other.data, (self, other), '+')
+
+    def _backward():
+      self.grad += 1.0 * out.grad
+      other.grad += 1.0 * out.grad
+    out._backward = _backward
+
+    return out
+
+
+  def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other) # Extension
+        out = Value(self.data * other.data, (self, other), '*')
+        
+        def _backward():
+            self.grad += out.grad * other.data # Bugfix
+            other.grad += out.grad * self.data # Bugfix
+        
+        out._backward = _backward
+        return out
+      
+
+  def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers (for now)"
+        out = Value(self.data ** other, (self,), f'**{other}')
+        
+        def _backward():
+            self.grad += other * (self.data ** (other - 1)) * out.grad
+        
+        out._backward = _backward
+        return out
+    # True division (treat as special multiplication)
+  def __truediv__(self, other): # self / other
+        return self * other**-1
+      
+  def __rmul__(self, other): # other * self
+        return self * other
+
+  def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self, ), 'exp')
+    
+        def _backward():
+            self.grad += out.data * out.grad
+        
+        out._backward = _backward
+        return out
+  def __radd__(self, other):
+      return self + other
+
+      
+  def log(self):
+    if self.data <= 0:
+      raise ValueError("Cannot calculate log for non-positive Value")
+    out = Value(log(self.data), (self,), 'log')
+    def _backward(): self.grad += (1 / self.data) * out.grad
+    out._backward = _backward
+    return out
+      
+
+  def __neg__(self):
+    return -1 * self
+
+
+  def backward(self): # exactly as in video
+    topo = []
+    visited = set()
+    def build_topo(v):
+      if v not in visited:
+        visited.add(v)
+        for child in v._prev:
+          build_topo(child)
+        topo.append(v)
+    build_topo(self)
+    self.grad = 1.0
+    for node in reversed(topo):
+      node._backward()
